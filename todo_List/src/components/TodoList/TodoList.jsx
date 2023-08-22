768d84665a4d04dacec4bc1a375e7832e24b5ea4
Veerbahadur Sen <veerbahadursen@gmail.com>
2023-08-22 21:43:27 +0530

    Todo-App

--- todo_List/src/components/TodoList/TodoList.jsx
+++ todo_List/src/components/TodoList/TodoList.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 import Todo from '../Todo/Todo';
 
-function TodoList() {
+function TodoList({list}) {
    
-    const [list,setList] =  useState([
-        {id:1,todoData:'todo 1'},
-        { id:2, todoData:'todo 2'}
-
-])
-
   return (
     <div> 
 
-    {list.length>0 && list.map(todo => <Todo key = {todo.id} todoData = {todo.todoData} /> )}
+    {list.length>0 && list.map(todo => <Todo 
+      key = {todo.id}
+      id={todo.id} 
+      isFinished={todo.finished} 
+      todoData = {todo.todoData} 
+     
+     /> )}
     </div>
   )
 }
